ee24bdd87266df955c76b9d4d38a767737c173e4
morain <1062254476@qq.com>
2021-09-11 17:43:12 +0800

    fix:fixbug

--- src/pages/Demos/Item/IframeBox.tsx
+++ src/pages/Demos/Item/IframeBox.tsx
@@ -1,20 +1,25 @@
 import { useEffect, useState } from "react"
 
+const origin = window.location.origin;
+// const origin = 'http://localhost:8001';
+
 export default ({ urlObj }) => {
   useEffect(() => {
     try {
       const iframe = document.getElementById('myframe');
-      iframe.contentWindow.postMessage(urlObj?.key, 'http://localhost:8001');
+      iframe.contentWindow.postMessage(urlObj?.key, origin);
     } catch (err) { }
   }, [urlObj?.key])
 
   if (!urlObj?.url) {
     return <></>
   }
+  // const urlss = `http://localhost:8001${urlObj?.url}`;
+  const urlss = urlObj?.url
 
   return (
     <>
-      <iframe scrolling="no" id='myframe' name='editor' height="100%" width='100%' frameBorder={0} src={urlObj?.url} />
+      <iframe scrolling="no" id='myframe' name='editor' height="100%" width='100%' frameBorder={0} src={urlss} />
     </>
   )
 }
